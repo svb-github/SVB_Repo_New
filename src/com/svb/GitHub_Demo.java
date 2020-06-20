4f0d268f7149f7e97f2f6455de3864bbb802807b
Sagar B <sagar_badve@yahoo.com>
2020-06-20 12:18:47 +0530

    This is first push from Sagar local to remote

--- src/com/svb/GitHub_Demo.java
+++ src/com/svb/GitHub_Demo.java
@@ -3,6 +3,7 @@ package com.svb;
 public class GitHub_Demo {
 	public static void main(String[] args) {
 		System.out.println("This is first GitHub Commit");
+		System.out.println("This is changed from local");
 	}
 
 }
