d9bd538f8c01bca04a0f3e7cc0a37d897820435a
Sagar B <sagar_badve@yahoo.com>
2020-06-20 12:43:45 +0530

    second push

--- src/com/svb/GitHub_Demo.java
+++ src/com/svb/GitHub_Demo.java
@@ -4,6 +4,7 @@ public class GitHub_Demo {
 	public static void main(String[] args) {
 		System.out.println("This is first GitHub Commit");
 		System.out.println("This is changed from local");
+		System.out.println("New change");
 	}
 
 }
